325532af79569c3f9e4d5e449688aa6693ddd214
Dmitry Mogilco <dmitry.mogilco@ecitele.com>
2019-01-07 16:06:35 +0200

    Apply Angular theme Add DatePicker

--- src/app/photos/photos-advanced-filter/photos-advanced-filter.component.ts
+++ src/app/photos/photos-advanced-filter/photos-advanced-filter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatChipInputEvent} from '@angular/material';
 
 @Component({
   selector: 'app-photos-advanced-filter',
@@ -10,6 +12,40 @@ export class PhotosAdvancedFilterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  tags: any[] = [
+    {name: 'Lemon'},
+    {name: 'Lime'},
+    {name: 'Apple'},
+  ];
+
+  addTag(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.tags.push({name: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(tag: any): void {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
   }
 
 }
